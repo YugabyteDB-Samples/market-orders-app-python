2cc26fbc9844939a41945c58417e9b0119e43ab0
Abhishek Mishra <geekabhi@outlook.com>
2022-08-12 16:53:16 +0530

    config: add table schema

---
+++ schema/schema_mysql.sql
@@ -0,0 +1,32 @@
+USE db;
+
+CREATE TABLE Buyer(
+    id integer NOT NULL AUTO_INCREMENT,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    age integer,
+    goverment_id text,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Trade(
+    id integer NOT NULL AUTO_INCREMENT,
+    buyer_id integer NOT NULL REFERENCES Buyer(id),
+    symbol text,
+    order_quantity integer,
+    bid_price float,
+    trade_type text,
+    order_time timestamp(0) DEFAULT NOW(),
+    PRIMARY KEY(id)
+);
+
+INSERT INTO Buyer(first_name, last_name,age, goverment_id) VALUES
+('John', 'Smith', 45, '7bfjd73'),
+('Arnold', 'Mazer', 55, 'unb23212'),
+('Lara', 'Croft', 35, '12338fb31'),
+('Patrick', 'Green', 42, 'asbn233'),
+('Anna', 'Romanoff', 46, 'klnnk3823'),
+('Alfred', 'Black', 55, '32345'),
+('San', 'Newman', 28, 'fjdks28943kd'),
+('Henry', 'McDonald', 31, 'dasdnouqbwe'),
+('Liza', 'Connor', 33, '1823bjkffe923');
