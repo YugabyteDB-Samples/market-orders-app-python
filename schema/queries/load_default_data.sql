3b5dcb6cf73e56f66d2d2dfe37b19470771ee513
Abhishek Mishra <geekabhi@outlook.com>
2022-08-16 21:23:03 +0530

    fix: remove unwanted schema clutter

---
+++ schema/queries/load_default_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO  public."User"(first_name,last_name,email) VALUES
+('Jennifer','Campos','jennifercampos@example.com'),
+('Thomas','Nelson','thomasnelson@example.com'),
+('David','Jordan','davidjordan@example.com'),
+('Sandra','Watson','sandrawatson@example.com'),
+('Holly','Ellis','hollyellis@example.com')
