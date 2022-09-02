a6b4a209f3f8f2cb69f9a5858e87ff61832a40d5
Abhishek Mishra <geekabhi@outlook.com>
2022-09-02 20:22:49 +0530

    add logging and exception handling

--- app/db.py
+++ app/db.py
@@ -1,3 +1,6 @@
+"""Database handler"""
+
+import logging
 from os import getenv
 from pathlib import Path
 
@@ -8,9 +11,7 @@ load_dotenv()  # Load environment variables from .env file
 
 # Database connection configuration
 db_config = {
-    "host": getenv(
-        "DB_HOST", "127.0.0.1"
-    ),
+    "host": getenv("DB_HOST", "127.0.0.1"),
     "port": getenv("DB_PORT", "5433"),
     "dbname": getenv("DB_NAME", "yugabyte"),
     "user": getenv("DB_USER", "admin"),
@@ -24,7 +25,7 @@ def database_connection():
         conn = psycopg2.connect(**db_config)
         return conn
     except Exception as e:
-        print(">>>>>>> Error connecting to database:", e)
+        logging.error("Error connecting to database:", e)
         exit(1)
 
 
@@ -33,36 +34,49 @@ def write_trade_to_db(connection, message, user_id):
     order_quantity = message["order_quantity"]
     trade_type = message["trade_type"]
     symbol = message["symbol"]
-    # trade_time = message['timestamp']
+    # trade_time = message['timestamp'] # TODO: should convert timestamp to datetime object instead
     bid_price = message["bid_price"]
     insert_query = f"insert into public.\"Trade\"(user_id, bid_price, order_quantity, trade_time, trade_type, symbol) values ('{user_id}','{bid_price}','{order_quantity}',NOW(),'{trade_type}','{symbol}');"
-    conn = database_connection()
+    conn = connection
     conn.set_session(autocommit=True)
     cur = conn.cursor()
-    cur.execute(insert_query)
-    cur.close()
+    try:
+        cur.execute(insert_query)
+        cur.close()
+    except Exception as e:
+        logging.error("Error executing query", e)
+        conn.close()
 
 
 def init_db():
-    """Initialize the database with default data"""
+    """Initialize the database with default schema"""
 
     # Read table schema from .sql file
-    create_sql_schema = Path("schema/schema.sql").read_text()
+    create_sql_schema_query = Path("schema/default_schema.sql").read_text()
+    # Uncomment next line if you want to load data from `load_default_data.sql` and not from `seed_user_data.py`
+    # load_default_data_query = Path("schema/queries/load_default_data.sql").read_text()
 
     # Create the database connection.
     conn = database_connection()
-    print(">>>>>>> Connecting to database...")
+    logging.info("Connecting to database...")
     # Open a cursor to perform database operations.
     # The default mode for psycopg2 is "autocommit=false".
     conn.set_session(autocommit=True)
     cur = conn.cursor()
 
     # Create the table & insert new rows into the database table
-    cur.execute(create_sql_schema)
-    print("Created schema with default data")
-    cur.close()
+    try:
+        cur.execute(create_sql_schema_query)
+        logging.info("Created table with default schema")
+        # Use the followingif you want load data from load_default_data.sql and not from `seed_user_data.py`
+        # cur.execute(load_default_data_query)
+        # logging.info("Inserted default user data")
+        cur.close()
+    except Exception as e:
+        logging.error("Error executing query", e)
+        conn.close()
 
 
 if __name__ == "__main__":
     connection = init_db()
-    print(">>>>>>> Connected to database...")
+    logging.info("Initializing database...")
